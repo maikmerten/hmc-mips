d560d2424dca6c8f59e50e0e13120fa656d8e2fd
carlpny <carlpny@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2006-12-24 18:48:03 +0000

    Added test 0.

---
+++ trunk/testing/test_000.asm
@@ -0,0 +1,23 @@
+# Harvey Mudd College VLSI MIPS Project
+# Spring, 2007
+#
+# Test 000
+#
+# Created: 12/24/06
+#
+# Commands tested:
+#   addi, add, beq, sw
+#
+# Expected Behavior:
+#   Fibinnaccie Sequence.  
+
+main:   addi $2, $0, 0          # initialize $2 = 0
+        addi $3, $0, 1          # initialize $3 = 1
+        addi $5, $0, 21         # initialize $5 = 21 (stopping point)
+loop:   add  $4, $2, $3         # $4 <= $2 + $3
+        add  $2, $3, $0         # $2 <= $3
+        add  $3, $4, $0         # $3 <= $4
+        beq  $4, $5, write      # when sum is 21, jump to write
+        beq  $0, $0, loop       # loop (beq is easier to assemble than jump)
+write:  sw   $4, 5($2)          # should write 21 @ 5 + 13 = 18
+end:    beq  $0, $0, end        # loop forever
