65a85f5fc4d8ec097ca77f3ff40d6c809a388758
twbarr <twbarr@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-01-29 01:11:21 +0000

    added tests for interrupting

--- trunk/testing/test_013.asm
+++ trunk/testing/test_013.asm
@@ -2,7 +2,7 @@
 # Thomas W. Barr, Carl Nygaard
 # Spring, 2007
 #
-# Test 010
+# Test 013
 #
 # Created: 1/20/07
 #
@@ -13,12 +13,30 @@
 
 # Start code 0x1FC00000
 main:   addi $3, $0, 0
-        nop               # run syscall (SPIM doesn't support this)
-        break 0		        # run the break call
-        nop               # modified in source code to be a malformed opcode (ffffffff)
-        add.s  $f2,$f2,$f4 	# floating point test        
+        nop                    # we don't really do much, we just sit around
+        nop                    # waiting to be interrupted
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
         
-        sw    $3, 0($0)         # should write 4 to address 0
+        sw    $3, 4($0)         # should write 1 to address 0
 end:    beq   $0, $0, end       # loop forever
         nop
 
@@ -29,8 +47,20 @@ except: nop
         addi $3, $3, 1          # increment the exception counter
         nop                     # MUST HAVE NOPS HERE - Move from cp0 takes a 
         nop                     # while, and has no hazard detection
+        nop                     # we just need to make sure we don't catch
+        nop                     # interrupts here
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
         nop
         addi  $7, $7, 4         # Point to one past the errant instruction
         jr    $7                # Resume
-        rfe                     # reenable interrupt handling
+        rfe                     # reenable interrupts
         
