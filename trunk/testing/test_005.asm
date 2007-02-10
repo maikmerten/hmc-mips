f859d77386d0eb4f3d82810eab9f86d6598a9dd5
carlpny <carlpny@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-02-10 22:49:04 +0000

    Likely final tweak for test 5 and memory address range fixes.

--- trunk/testing/test_005.asm
+++ trunk/testing/test_005.asm
@@ -18,7 +18,8 @@ main:   li    $3, 0xbfc00018    # $3 = 0xbfc00018
                                 # (this is r3dest)
         addi  $2, $2, 1         # Branch delay slot, so $2++ = 1
         addi  $2, $2, 0x10      # Should not be taken
-r3dest: #jal   j1                # jump to j1 (0x9 << 2) and put 0x20 in $31
+r3dest: #jal   j1               # jump to j1 (0x9 << 2) and put 
+                                # 0xbfc00020 in $31
         # Hardcode the instruction, the assembler wants to make it PIC
         #.word   0x0c000008 # This location doesn't account for cache
         .word   0x0cff0009
@@ -33,7 +34,7 @@ b1:     bgtz  $7, b2            # Should be taken
         addi  $2, $2, 0x40      # Should not be taken
 b2:     addu  $3, $0, $0        # $3 = 0
         bgez  $3, b3            # Should be taken
-        add   $3, $31, $7       # $3 = 0x10 + 0x1c = 0x2c
+        add   $3, $31, $7       # $3 = 0xbfc00020 + 0x4 = 0xbfc00024
         addi  $2, $2, 0x80      # Should not be taken
 b3:     lui   $4, 0x8000       # $3 = 0x80000000
         bltzal $4, b4           # $31 = address X, with branch taken
@@ -51,6 +52,6 @@ b6:     blez  $4, b8            # Should be taken
         addi  $2, $2, 1         # Should be added (BDS)
 b7:     jr    $31               # Jump to b6
         nop                     # BDS
-b8:     sw    $2, 0($3)         # should write 9 to address 0x2c
+b8:     sw    $2, 0($3)         # should write 9 to address 0xbfc00024
 end:    beq   $0, $0, end       # loop forever
         nop
