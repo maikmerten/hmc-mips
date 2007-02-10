9b667aedcf9911d01da1a63e854335f7ca23edc5
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-02-10 11:25:06 +0000

    Added a source file for testing the output of mips gcc for floating point

---
+++ trunk/testing/test_fp_1.c
@@ -0,0 +1,19 @@
+/* Harvey Mudd CMOS VLSI class MIPS project
+ * 
+ * $Author$
+ * Created 2/10/07 by Matt McKnett
+ * 
+ * $Date$
+ * test_fp_1.c
+ *
+ * This program is designed to test the "soft" floating point routines
+ * of GCC to ensure that code is generated properly to handle floating
+ * point in software (since it is not implemented on this chip in hardware.)
+ */
+
+sm("li $sp,0xf0");
+
+void test_fp_1()
+{
+
+}
