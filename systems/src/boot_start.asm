d246959d7e8b6bdaf6a2593f7deb9895a86e750f
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-02-27 03:50:09 +0000

    The initial boot code needs to be written solely in assembly.

--- systems/src/boot_start.asm
+++ systems/src/boot_start.asm
@@ -1,33 +1,18 @@
-	.file	1 "boot_start.c"
-	.section .mdebug.abi32
-	.previous
- #APP
-	lui $t0, 0xbfc0
-	ori $t0, $t0, 0xbeef
-	jr  $t0
- #NO_APP
-	.text
-	.align	2
-	.globl	dummyfunc
-	.ent	dummyfunc
-dummyfunc:
-	.frame	$fp,8,$31		# vars= 0, regs= 1/0, args= 0, gp= 0
-	.mask	0x40000000,-8
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	
-	addiu	$sp,$sp,-8
-	sw	$fp,0($sp)
-	move	$fp,$sp
-	move	$sp,$fp
-	lw	$fp,0($sp)
-	addiu	$sp,$sp,8
-	j	$31
-	nop
+# Harvey Mudd College VLSI MIPS Project
+# Matt McKnett
+# Spring, 2007
+# 
+# Bootloader bootstrap code
+#
+# This code is step 1 in the bootloading process.  It is designed to jump
+# to the main boot code that is stored in ROM.  This keeps the bootloader code
+# from running into exception entry points and gives us a simple test of CPU
+# function (according to See MIPS Run page 110). 
 
-	.set	macro
-	.set	reorder
-	.end	dummyfunc
-	.size	dummyfunc, .-dummyfunc
-	.ident	"GCC: (GNU) 4.1.1"
+# We don't want the assembler to reorder or fill branch delay slots.
+.set noreorder
+
+boot:	lui $t0, 0x0400		# Our main code is located at 0x04001018
+	ori $t0, $t0, 0x1018
+	jr  $t0
+	nop
