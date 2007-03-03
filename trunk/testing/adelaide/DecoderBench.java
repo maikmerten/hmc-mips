8a8b106e30197b3850cca20729cef3d6afcd14d2
joel.stan <joel.stan@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-03-03 08:07:32 +0000

    Updated decoder64 code (Joel)

--- trunk/testing/adelaide/DecoderBench.java
+++ trunk/testing/adelaide/DecoderBench.java
@@ -4,7 +4,7 @@ public class DecoderBench
 {
 	public static void main(String[] args) throws Exception
 	{
-		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("decoder_bench.cmd")));
+		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("test_decoder64.cmd")));
 		writer.println("stepsize 2ns");
 		writer.println("vector a a[5] a[4] a[3] a[2] a[1] a[0]");
 		writer.println("vector y y[63] y[62] y[61] y[60] y[59] y[58] y[57] y[56] y[55] y[54] y[53] y[52] y[51] y[50] y[49] y[48] y[47] y[46] y[45] y[44] y[43] y[42] y[41] y[40] y[39] y[38] y[37] y[36] y[35] y[34] y[33] y[32] y[31] y[30] y[29] y[28] y[27] y[26] y[25] y[24] y[23] y[22] y[21] y[20] y[19] y[18] y[17] y[16] y[15] y[14] y[13] y[12] y[11] y[10] y[9] y[8] y[7] y[6] y[5] y[4] y[3] y[2] y[1] y[0]");
