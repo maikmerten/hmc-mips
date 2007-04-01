97179884711b1209235a885ed2249fb1f35a0db7
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-01 09:29:12 +0000

    Added source files for our lightsOut demo program.

---
+++ trunk/systems/src/lightsOut.h
@@ -0,0 +1,19 @@
+/*
+ * lightsOut.h
+ * 
+ * Created by Matt McKnett on 4/1/07
+ * VLSI Spring 2007
+ * 
+ * This is a demo program for the hmc-mips project.  It is a
+ * "lights out" game playable on the LCD display onboard the
+ * PCB board.
+ */
+
+#ifndef LIGHTSOUT_INCLUDED
+#define LIGHTSOUT_INCLUDED 1
+
+
+
+
+
+#endif /* LIGHTSOUT_INCLUDED. */
