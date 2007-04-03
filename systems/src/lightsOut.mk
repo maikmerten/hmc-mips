2fd2cc49e63607209c504d3848de453f62856913
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-03 11:58:45 +0000

    Made some attempts at setting gp in compiler flags to no avail.

--- systems/src/lightsOut.mk
+++ systems/src/lightsOut.mk
@@ -7,7 +7,7 @@ lightsOut.asm: lightsOut.c lightsOut.h muddCLib/muddCLib.h muddCLib/mtRand.h
 	$(CC) $(CFLAGS) -S lightsOut.c -o lightsOut.asm
 
 lightsOut.out: lightsOut.o muddCLib/muddCLib.o muddCLib/mtRand.o
-	$(LD) -EL -Ttext=$(PROG_LOC) -o lightsOut.out lightsOut.o muddCLib/muddCLib.o muddCLib/mtRand.o
+	$(LD) $(LDFLAGS) -Ttext=$(PROG_LOC) -o lightsOut.out lightsOut.o muddCLib/muddCLib.o muddCLib/mtRand.o
 
 lightsOut.exe: lightsOut.c lightsOut.h muddCLib/muddCLib.h muddCLib/mtRand.h
 	gcc -o lightsOut lightsOut.c muddCLib/muddCLib.c muddCLib/mtRand.c
--- systems/src/test_leds.c
+++ systems/src/test_leds.c
@@ -1,28 +1,26 @@
 #include "muddCLib/muddCLib.h"
 
+char dip_switch[4];
+
 int main() {
-	char switch0;
-	char switch1;
-	char switch2;
-	char switch3;
 
 	while(1)
 	{
 
-	switch0 = 0x1;
-	switch1 = 0x2;
-	switch2 = 0x4;
-	switch3 = 0x8;
+	dip_switch[0] = 0x1;
+	dip_switch[1] = 0x2;
+	dip_switch[2] = 0x4;
+	dip_switch[3] = 0x8;
 
 	if(readSwitch(SWITCH0) == SWITCH_ON)
-		switch0 = 0;
+		dip_switch[0] = 0;
 	if(readSwitch(SWITCH1) == SWITCH_ON)
-		switch1 = 0;
+		dip_switch[1] = 0;
 	if(readSwitch(SWITCH2) == SWITCH_ON)
-		switch2 = 0;
+		dip_switch[2] = 0;
 	if(readSwitch(SWITCH3) == SWITCH_ON)
-		switch3 = 0;
-	setLED((char)(switch0 + switch1 + switch2 + switch3));
+		dip_switch[3] = 0;
+	setLED((char)(dip_switch[0] + dip_switch[1] + dip_switch[2] + dip_switch[3]));
 
 	}
 	return 0;
