08903c1028fdb0619b7f4e0bf6e43df8777ff86c
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-05 01:50:57 +0000

    Updated test for test_lcd.  This test will attempt to move a star around the screen and then print the Hello World message on two lines.

--- systems/src/test_lcd.c
+++ systems/src/test_lcd.c
@@ -3,25 +3,61 @@
 int main() 
 {
 	char str1[16] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+	char str2[16] = {' ', ' ', ' ', 'H', 'M', 'C', ' ', 'V', 'L', 'S', 'I', ' ', '0', '7'};
 	int i;
-	//char str2[16] = {' ', ' ', ' ', 'H', 'M', 'C', ' ', 'V', 'L', 'S', 'I', ' ', '0', '7'};
 
 	initLCD();
-	
-	//dispChar('F');
 
-	for(i = 0; i < 13; ++i)
+	while(1)
 	{
-		//move((char)i);
-		dispChar(str1[i]);
+		// Move test
+		move(L_secondRow);
+		dispChar('*');
+
+		delay1KTCYx(1000);
+
+		sendInst(L_clear);
+		move(0x08);
+		dispChar('*');
+
+		delay1KTCYx(1000);
+
+		// Print "Hello, Word!"
+		//       "   HMC VLSI 07"
+
+		sendInst(L_clear);
+		move(0x00);
+
+		for(i = 0; i < LCD_WIDTH; ++i)
+		{
+			if(str1[i] == '\0')
+				break;
+			dispChar(str1[i]);
+		}
+
+		move(L_secondRow);
+
+		for(i = 0; i < LCD_WIDTH; ++i)
+		{
+			if(str2[i] == '\0')
+				break;
+			dispChar(str2[i]);
+		}
+
+		delay1KTCYx(10000);
+
+		sendInst(L_clear);
+
+		delay1KTCYx(1000);
 	}
 
+	/* for some reason, dispMessage doesn't work. */
 	//dispMessage(str1, str2);
 	
 	/* Turn off the cursor. */
-	sendInst(L_disp);
+	//sendInst(L_disp);
 
-	while(1); // Loop forever
+	//while(1); // Loop forever
 
 	return 0;
 }
--- systems/src/test_lcd.mk
+++ systems/src/test_lcd.mk
@@ -3,7 +3,7 @@
 # Defines the dependencies for making test_lcd
 
 test_lcd.asm: test_lcd.c muddCLib/muddCLib.h boot_start.dat boot_loader.dat
-	$(CC) $(CFLAGS) -S test_lcd.c -o test_lcd.asm
+	$(CC) $(CFLAGS) -O2 -S test_lcd.c -o test_lcd.asm
 
 test_lcd.out: test_lcd.o muddCLib/muddCLib.o
 	$(LD) $(LDFLAGS) -Ttext=$(PROG_LOC) -o test_lcd.out test_lcd.o muddCLib/muddCLib.o
