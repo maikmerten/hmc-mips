850401e946fd2e188bb7e315ba6c68315e85c7ce
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-11 05:11:43 +0000

    The makeCharArrayFromString.py script makes arrays of characters out of strings for copying and pasting into C code.  Pipe in input from stdin, and it puts output on stdout.  The only problem is that it doesn't automatically add in the final '\0' because python likes to do '\\0' instead.

--- systems/src/makeCharArrayFromString.py
+++ systems/src/makeCharArrayFromString.py
@@ -16,10 +16,26 @@ def strToList(stringin):
 
 strCnt = 0
 
+print "Don't forget to add null terminators to strings! \
+    An extra char is included in the array for this purpose.\n\n"
+
 for param in sys.stdin:
-    paramLength = len(param)
-    outputString = str(strToList(param))
+    # Don't interpret newlines.
+    outputString = param.replace("\n","").replace("\r","")
+    strLength = len(outputString) + 1
+
+    # Turn the string into a string list.
+    outputList = strToList(outputString)
+
+    # Add the null terminator to the list.  Python likes to print \\0 and I
+    # can't make it stop.
+    #outputList.append("\x5C" + "0")
+    
+    outputString = str(outputList)
+    
+    # Change [ to {, ] to }
     outputString = outputString.replace('[','{').replace(']','}')
-    outputString = "char str%d[%d] = " % (strCnt, paramLength) + outputString
+    outputString = "char str%d[%d] = " % (strCnt, strLength) + outputString
+    outputString += ";"
     print outputString
     strCnt += 1
