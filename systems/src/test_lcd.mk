1b870dc08566056a2971e609e6aeaac3c215cc06
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-13 01:33:37 +0000

    Modified test_lcd.mk to use some automatic variables, added an almost exact copy called test_buttons.mk to build test_buttons, modified the Makefile to support the new test_buttons, and built a new muddCLib.o.

--- systems/src/test_lcd.mk
+++ systems/src/test_lcd.mk
@@ -3,12 +3,12 @@
 # Defines the dependencies for making test_lcd
 
 test_lcd.asm: test_lcd.c muddCLib/muddCLib.h boot_start.dat boot_loader.dat
-	$(CC) $(CFLAGS) -O2 -S test_lcd.c -o test_lcd.asm
+	$(CC) $(CFLAGS) -O2 -S $< -o $@ 
 
 test_lcd.o: test_lcd.asm
 	python checkInstructions.py $<
 	$(AS) -EL -o $@ $<
 
 test_lcd.out: test_lcd.o muddCLib/muddCLib.o
-	$(LD) $(LDFLAGS) -Ttext=$(PROG_LOC) -o test_lcd.out test_lcd.o muddCLib/muddCLib.o
+	$(LD) $(LDFLAGS) -Ttext=$(PROG_LOC) -o $@ $^
 
