9c9598a26c848e33cb3363f10f2d2182fd773287
whiterook2004 <whiterook2004@26d96d29-3b25-0410-87a5-e19d1b16b1e7>
2007-04-16 06:55:16 +0000

    Applied keywords to the source files

--- systems/src/test_simple.c
+++ systems/src/test_simple.c
@@ -1,7 +1,7 @@
 /* test_simple.c
  *
  * Created by Matt McKnett, HMC-MIPS Project, VLSI Spring 2007
- * $Author: whiterook2004 $,  $Date: 2007-04-12 17:39:50 -0700 (Thu, 12 Apr 2007) $ -- $Revision: 542 $
+ * $Author$,  $Date$ -- $Revision$
  * 
  * This test just counts up on the LEDs.  Its purpose is to give the testers something to run
  * while they are determining what clock frequency is best for the chip.
